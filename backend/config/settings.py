976e9b5b266b797f7ed2067a1d5a767e422151c0
sohail-the-highlight <101591362+sohail-the-highlight@users.noreply.github.com>
2025-06-10 15:53:35 +0530

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@6q&@=vmlnkexbyfa3nb^b6u94h7c0yogm&086l%&0ggi=x%)m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['budget-tracker-app-dp7u.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
