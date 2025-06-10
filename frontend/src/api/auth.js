bc16833618f6966d52adab9808ba4d27ac101737
sohail-the-highlight <101591362+sohail-the-highlight@users.noreply.github.com>
2025-06-10 16:07:46 +0530

    Update auth.js

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api/auth';
+const API_URL = 'https://budget-tracker-app-dp7u.onrender.com/api/auth';
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login/`, {
