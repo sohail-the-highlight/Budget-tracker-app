168015407c2dd2bed494e63e0cfb86195820165d
sohail-the-highlight <101591362+sohail-the-highlight@users.noreply.github.com>
2025-06-10 16:08:40 +0530

    Update budget.js

--- frontend/src/api/budget.js
+++ frontend/src/api/budget.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://budget-tracker-app-dp7u.onrender.com/api';
 
 export const updateTransaction = async (token, transactionId, updatedData) => {
   const response = await axios.put(`${API_URL}/transactions/${transactionId}/`, updatedData, {
