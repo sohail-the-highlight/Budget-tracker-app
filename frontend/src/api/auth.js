bece9c6c1089a962259e8cd5466d7a87fa3c71af
sohail-the-highlight <101591362+sohail-the-highlight@users.noreply.github.com>
2025-06-12 11:23:19 +0530

    Update auth.js

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 //trial update
-const API_URL = '${process.env.REACT_APP_API_BASE_URL}/api/auth';
+const API_URL = `${process.env.REACT_APP_API_BASE_URL}/api/auth`;
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login/`, {
