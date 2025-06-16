c292445280d4be99819e4c3b7899eb8a02268200
sohail-the-highlight <101591362+sohail-the-highlight@users.noreply.github.com>
2025-06-16 13:03:41 +0530

    Update settings.py
    debug falsed

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@6q&@=vmlnkexbyfa3nb^b6u94h7c0yogm&086l%&0ggi=x%)m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['budget-tracker-app-dp7u.onrender.com', 'localhost', '127.0.0.1']
 
